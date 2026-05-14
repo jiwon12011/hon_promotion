2f4179c7f78154b67b151c7f68c757bddfe9a71c
서지원 <jiwon12011@gmail.com>
2026-05-14 10:57:00 +0900

    Configure GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,54 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
-module.exports = {
-  entry: path.resolve(__dirname, "src/main.js"),
-  output: {
-    filename: "assets/js/[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    assetModuleFilename: "assets/media/[name].[contenthash][ext]"
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: path.resolve(__dirname, "src/main.js"),
+    output: {
+      filename: "assets/js/[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      publicPath: isProduction ? "/hon_promotion/" : "/",
+      assetModuleFilename: "assets/media/[name].[contenthash][ext]"
     },
-    extensions: [".js", ".vue", ".json"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader"
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src")
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|jpe?g|gif|webp|svg|mp4|webm)$/i,
-        type: "asset/resource"
+      extensions: [".js", ".vue", ".json"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|jpe?g|gif|webp|svg|mp4|webm)$/i,
+          type: "asset/resource"
+        }
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public/index.html")
+      })
+    ],
+    devServer: {
+      host: "127.0.0.1",
+      port: 5173,
+      hot: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.resolve(__dirname, "public")
       }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html")
-    })
-  ],
-  devServer: {
-    host: "127.0.0.1",
-    port: 5173,
-    hot: true,
-    historyApiFallback: true,
-    static: {
-      directory: path.resolve(__dirname, "public")
     }
-  }
+  };
 };
